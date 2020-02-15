f7f8aa5a2c8191d38774963d59dbbfd525af1e41
Dzmitry Samoila <dzsamoila@gmail.com>
2020-02-15 01:49:42 -0500

    Add files via upload
    ready

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -3,8 +3,13 @@ Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) 
 {
+    // ==split()== method to return a new array
+    // ==reverse()== method to reverse the new created array
+    // ==join()== method to join all elements of the array into a string
     return someStr.split("").reverse().join("");
+   
 }
 
 
 console.log(reverseStr("someThing") ) ; 
+
