531c767b835e42afcf56676b60f6b5895bcbe131
Dzmitry Samoila <dzsamoila@gmail.com>
2020-02-15 01:21:20 -0500

    Add files via upload

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,18 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
+function fib(n) 
+{
+    if (n <= 1) 
+      { return 1 }
+
+      else 
+      {   
+           return fib(n - 1) + fib(n - 2) 
+      }
 
 }
+
+
+
+console.log(  fib(11) ) ;
