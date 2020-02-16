3c5b809f81281395eeafe6d0b8cb8f0503d45a79
Mr Dzen <dzsamoila@gmail.com>
2020-02-16 17:54:14 -0500

    Done

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -3,4 +3,21 @@ Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
   
+    var re = /[^A-Za-z0-9]/g; // regular expression's pattern
+    someStr = someStr.toLowerCase().replace(re, '');
+    var len = someStr.length;
+    for (var i = 0; i < len/2; i++) 
+    {
+      if (someStr[i] !== someStr[len - 1 - i]) 
+        {
+        return false;
+        } //if
+    } //for
+ return true;
 }
+
+
+
+
+
+console.log("Is Palyndrome ? " + isPalindrome("Abba"));
