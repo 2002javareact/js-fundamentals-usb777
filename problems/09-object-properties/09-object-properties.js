a1b3b656bb9aaf595208091be3653035a5aca1c6
Mr Dzen <dzsamoila@gmail.com>
2020-02-16 17:56:00 -0500

    Done

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -3,4 +3,17 @@ Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
 
-}
+    // it will grab all of the names of the fields in that object
+// this means its getting the keys, not the values
+  for(let key in someObj)
+    {
+        console.log(key+" : "+someObj[key]);
+    }
+
+} 
+
+
+
+
+
+
