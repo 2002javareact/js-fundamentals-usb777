bd7a77991feff7cc16cac1956dc3cc9f145688f6
usb777 <dzsamoila@gmail.com>
2020-02-17 16:28:12 -0500

    Done

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -102,20 +102,3 @@ printShape("Triangle", 4, "*");
 printShape("Square", 5, "@");
 
 printShape("Diamond", 6, "*");
-
-/*
-var stars='';
-var width = 11;
-var num = (width+1)/2;
-for (let i = num-1; i >-num; i--) {
-for (let j = num-Math.abs(i); j < num; j++) {
-stars+=' '
-}
-for (let j = 0; j < 2*(num-Math.abs(i))-1; j++) { //2*num-(2*Math.abs(i) +1) simplified to 2*(num-Math.abs(i))-1
-stars+="*";
-}
-stars+="\n";
-}
-console.log(stars);
-
-*/
